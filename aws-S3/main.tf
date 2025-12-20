be5cf63bbceee683deb6913c5ebb2bc1263c7e3c
JEYA NAGARAJA J <jjeyanagaraja8602@gmail.com>
2025-12-20 12:40:55 +0530

    Corrected the tag block

--- aws-S3/main.tf
+++ aws-S3/main.tf
@@ -6,6 +6,6 @@ resource "aws_s3_bucket" "awsS3" {
     bucket=format("%s-%s",var.bucketName,random_id.randomHex.hex)
     tags = {
       Name="S3-Tf-via-Jenkins"
-      Environment=Dev
+      Environment="Dev"
     }
 }
